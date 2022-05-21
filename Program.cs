da63cf2bfaf7879ed7fde38469dab9d6b7f898f2
sangsq <sang93@qq.com>
2022-05-21 22:28:14 +0800

    优化一下输出信息和代码

--- Program.cs
+++ Program.cs
@@ -42,30 +42,29 @@ namespace MyApp
                 )
             };
 
-            HttpClient client = new HttpClient();
+            var client = new HttpClient();
 
             // 不要将中文转义了
             var options = new JsonSerializerOptions { Encoder = JavaScriptEncoder.Create(UnicodeRanges.All) };
 
             var user = new List<StudyInfo>();
-            var ishave = new List<string>();
             foreach (var item in apiInfo)
             {
                 string jsoninfo = await client.GetStringAsync(item.url);
                 JsonNode jsonNode = JsonNode.Parse(jsoninfo)!;
                 // 合集总课程数
-                int classnum = (int)jsonNode["totalScoreUnits"];
-                foreach (JsonNode uinfo in jsonNode["results"].AsArray())
+                int classnum = (int)jsonNode["totalScoreUnits"]!;
+                foreach (var uinfo in jsonNode["results"]!.AsArray())
                 {
                     // 学完的用户
-                    if ((float)uinfo["score"] == classnum)
+                    if ((float)uinfo["score"]! == classnum)
                     {
                         // 看看有没有这个用户的信息
-                        var temp = user.FirstOrDefault(e => e.userName == (string)uinfo["userDisplayName"]);
+                        var temp = user.FirstOrDefault(e => e.userName == (string)uinfo["userDisplayName"]!);
                         if (temp is null)
                         {
                             // 初始创建这个用户
-                            user.Add(new StudyInfo((string)uinfo["userDisplayName"], 1, new List<string>() { item.title }));
+                            user.Add(new StudyInfo((string)uinfo["userDisplayName"]!, 1, new List<string>() { item.title }));
                         }
                         else
                         {
@@ -84,8 +83,9 @@ namespace MyApp
             Console.WriteLine($"完成《C# 学习》{user.Where(x=>x.learnName.IndexOf("C# 学习")>-1).Count()}人");
             Console.WriteLine($"完成《ASP.NET Core 开发》{user.Where(x => x.learnName.IndexOf("ASP.NET Core 开发") > -1).Count()}人");
             Console.WriteLine($"完成《.NET 移动应用》{user.Where(x => x.learnName.IndexOf(".NET 移动应用") > -1).Count()}人");
-            Console.WriteLine($"完成 2 次挑战 {user.Where(x => x.learnNum ==2).Count()}人");
-            Console.WriteLine($"完成 3 次挑战 {user.Where(x => x.learnNum == 3).Count()}人\n 分别是");
+            Console.WriteLine($"仅完成 1 次挑战 {user.Where(x => x.learnNum == 1).Count()}人");
+            Console.WriteLine($"仅完成 2 次挑战 {user.Where(x => x.learnNum ==2).Count()}人");
+            Console.WriteLine($"共完成 3 次挑战 {user.Where(x => x.learnNum == 3).Count()}人\n 分别是");
 
             foreach (var item in user.Where(x => x.learnNum == 3)) {
                 Console.WriteLine(item.userName);
